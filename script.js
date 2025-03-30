217db3f703f0f4514fff7a0070150b6eaa5eb3b0
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2025-03-30 15:18:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const recipes = [
+    {
+        title: "Banana Shake",
+        description: "A thick and heavy shake with fruits and nuts",
+        category: "Drink",
+        ingredients: [
+            { name: "Banana", quantity: "2" },
+            { name: "Milk", quantity: "500ml" },
+            { name: "Nuts", quantity: "100g" },
+            { name: "Hersheys", quantity: "To taste" },
+            { name: "Fruits", quantity: "50g" }
+        ]
+    },
+    {
+        title: "Maggi",
+        description: "A delicious atta noodles with vegetables",
+        category: "Breakfast",
+        ingredients: [
+            { name: "Banana", quantity: "2" },
+            { name: "Milk", quantity: "500ml" },
+            { name: "Nuts", quantity: "100g" },
+            { name: "Hersheys", quantity: "To taste" },
+            { name: "Fruits", quantity: "50g" }
+        ]
+    }
+];
